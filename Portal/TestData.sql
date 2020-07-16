28fee0d19f48f453660d0f7f1b29b5c8b9146f04
Your Name <you@example.com>
2020-07-16 18:36:56 +0900

    add new file

--- Portal/TestData.sql
+++ Portal/TestData.sql
@@ -14,23 +14,30 @@ insert into 権限マスタ values (1, '管理者');
 create table ユーザーマスタ (ユーザーid varchar(20) primary key, 名前 text, パスワード text, 権限id numeric, 削除フラグ numeric, 更新日時 timestamp not null default current_timestamp, foreign key (権限id) references 権限マスタ (権限id));
 
 insert into ユーザーマスタ values ('aiueo', '田中太郎', '$2a$11$p58noHMznd5kTgqbeo59ruq4qgjbMV8giXsVF8clis.iBj/EGH9b2', 1, 0); 
+insert into ユーザーマスタ values ('mmm', '半間有次郎', '$2a$11$r7Qa7OF/3aPFzUpCW2DUaO6/d0U3q44iKjcvivvu94lMvkK1hoJLO', 1, 0); 
+insert into ユーザーマスタ values ('kkk', '高田秀樹', '$2a$11$g4Mcx6X2HVYxu77qoz21oe1iDOzNf5GfT8nQMZMMwDNyHxXJetvRu', 0, 0); 
 
 
 
 /* メールテーブル */
 create table メールテーブル (送信者ユーザーid varchar(20), 受信者ユーザーid varchar(20), 件名 text, メッセージ text, 更新日時 timestamp not null default current_timestamp, foreign key (受信者ユーザーid) references ユーザーマスタ (ユーザーid), foreign key (送信者ユーザーid) references ユーザーマスタ (ユーザーid));
 
+insert into メールテーブル values ('aiueo', 'mmm', '会議の件について', '明日はお願いします'); 
+insert into メールテーブル values ('aiueo', 'kkk', '初めまして', 'よろしく'); 
+insert into メールテーブル values ('mmm', 'aiueo', '来週の会議の件', '遅刻厳禁'); 
+
+
 
 
 /*　勤怠テーブル */
 create table 勤怠テーブル (ユーザーid varchar(20), 勤務開始時間 timestamp, 勤務終了時間 timestamp, 休憩時間 numeric, 更新日時 timestamp not null default current_timestamp, foreign key (ユーザーid) references ユーザーマスタ (ユーザーid));
 
+insert into 勤怠テーブル values ('aiueo', '2019-02-02 09:00:00', '2019-02-02 17:45:00', 45); 
+
 
 
 /* 予定テーブル */
 create table 予定テーブル (予定id smallserial primary key,ユーザーid varchar(20), 予定内容 varchar(10), 開始日時 timestamp, 終了日時 timestamp, 更新日時 timestamp not null default current_timestamp, foreign key (ユーザーid) references ユーザーマスタ (ユーザーid));
-
-insert into 予定テーブル(ユーザーid,予定内容,開始日時,終了日時) values ('aiueo', '会議', '2020-03-31 09:00:00', '2020-03-31 17:45:00'); 
 
 
 
@@ -45,7 +52,7 @@ insert into 施設テーブル(施設名,開放開始時間,開放終了時間) values ('休憩室', '20
 
 
 /* 施設利用状況テーブル */
-create table 施設利用状況テーブル (ユーザーid varchar(20), 施設id smallint, 予定id smallint, 更新日時 timestamp not null default current_timestamp, , foreign key (ユーザーid) references ユーザーマスタ (ユーザーid), foreign key (施設id) references 施設テーブル (施設id));
+create table 施設利用状況テーブル (ユーザーid varchar(20), 施設id smallint, 予定id smallint, 更新日時 timestamp not null default current_timestamp,  foreign key (ユーザーid) references ユーザーマスタ (ユーザーid), foreign key (施設id) references 施設テーブル (施設id));
 
 
 
